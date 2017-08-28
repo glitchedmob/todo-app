fc9ad16e5f261c231556145fb54eada7beda180f
Levi Zitting <levizitting5663@gmail.com>
2017-08-28 00:37:31 -0500

    Starting to build out api. Adding basic auth as well

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('notes', 'NoteController@index');
+Route::post('notes', 'NoteController@create');
+Route::get('notes/{id}', 'NoteController@show');
+Route::patch('notes/{id}', 'NoteController@update');
+Route::delete('notes/{id}', 'NoteController@destroy');
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return view('index');
+});
+
+Auth::routes();
+
+Route::middleware('auth')->get('app', function () {
+    return view('app');
 });
